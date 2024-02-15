fcc11bc6533b4207424ff0055b3157cc196368c0
OlcottEthan <156358035+OlcottEthan@users.noreply.github.com>
2024-02-15 09:03:13 -0700

    Update and rename Results_team.R to team_results.R
    Corrected the function to return all necessary values.

---
+++ R/team_results.R
@@ -0,0 +1,40 @@
+#' Gives results for a specified team
+#'
+#' This will calculate the number of wins, losses, win percentage, number of games at home,
+#' total games played and percentage of games played at home.
+#'
+#' @param team This is the desired team to generate statistics for.
+#' 
+#' @return This function returns a list containing the number of wins, losses,
+#' win percentage, home games, total games played and percentage of games at home
+#' for the specified team.
+#'
+#' @examples
+#' ## Give win and home game statistics for 'Southern Utah'
+#' Results_team('Southern Utah')
+#' 
+#' @import
+#'   tidyverse
+#'   magrittr
+#'
+#' @export
+
+Results_team <- function(team) {
+  
+  team_results <- team_filter(team) |>
+    mutate(team = team,
+           home = ifelse(Away == team,FALSE,TRUE),
+           result = case_when(score_diff > 0 & (Away == team) ~ 'win',
+                              score_diff < 0 & (Home == team) ~ 'win',
+                              score_diff == 0 ~ 'tie',
+                              TRUE ~ 'loss'),
+           home_game = ifelse(Home == team,TRUE,FALSE)) |>
+    summarize(win = sum(result == 'win'),
+             loss = sum(result == 'loss'),
+             total_games = length(home)) |>
+    mutate(home_percentage = home_games/total_games,
+           win_percentage = win/total_games) |>
+    select(team,win,loss,win_percentage,home_games,total_games,home_percentage)
+  
+return(team_results)
+}
