ab0b7ad2e2bbaaba46a82143d5cf0b50b12bed41
OlcottEthan <156358035+OlcottEthan@users.noreply.github.com>
2024-02-15 10:02:03 -0700

    Update barplot_win_vs_homestatus.R
    Updated the function to plot the results for a specified team only

--- R/barplot_win_vs_homestatus.R
+++ R/barplot_win_vs_homestatus.R
@@ -1,22 +1,24 @@
-#' Gives a quick barplot of number of wins.
+#' Gives a quick barplot of number of wins for a specified team
 #'
-#' This will return a barplot of the number of wins vs traveling status.
+#' This will return a barplot of the number of wins vs traveling status for the team specified.
 #'
+#' @param team This is the desired team to generate plot for.
 #' 
-#' @return This function returns a split barplot with the number of games played,
+#' @return This function returns a split barplot based on the team with the number of games played,
 #' split whether those wins were at a home event or away.
 #'
 #' @examples
-#' ## Give a barplot of number of wins with split on traveling or home games
-#' barplot_win_vs_homestatus()
+#' ## Give a barplot of number of wins with split on traveling or home games for Utah
+#' barplot_win_vs_homestatus('Utah')
 #' 
 #' @import
-#'   ggplot2
+#' ggplot2
 #'
 #' @export
 
-barplot_win_vs_homestatus <- function(){
-  cbbga24 |>
+barplot_win_vs_homestatus <- function(team){
+  team_filter(team) |>
+    filter((Away == team & score_diff>0) | (Home == team & score_diff<0)) |>
     mutate(win_type = case_when(score_diff > 0 ~ 'Away',
                                 score_diff < 0 ~ 'Home',
                                 TRUE ~ 'tie')) |>
